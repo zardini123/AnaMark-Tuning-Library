519c5fa45bc4791a2a2a3c1b14b9516c550e1226
Taras A. Palczynski III <tp3@Tarass-iMac.attlocal.net>
2020-08-10 23:09:29 -0400

    Exported enviroment variable for value to work

--- tests/testutils.cpp
+++ tests/testutils.cpp
@@ -4,12 +4,11 @@
 std::string TestUtils::testFile(std::string fn) {
     const char* dir = getenv( "ANAMARK_TEST_DATA_DIRECTORY" );
     std::string td;
-    if( ! dir )
+    if (dir == nullptr)
         td = "tests/data/";
     else
         td = std::string( dir );
 
-            
+
     return std::string( td ) + fn;
 }
-
